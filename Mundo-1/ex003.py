28cabfebe216b10f913c1bbbd384bd6a1bc8350a
Gabriel Leão da Silva <gabriellsilva1200@outlook.com.br>
2021-04-20 22:05:25 -0300

    Create ex003.py

---
+++ Mundo-1/ex003.py
@@ -0,0 +1,4 @@
+numero1 = int(input('\033[1;97mDigite um número: '))
+numero2 = int(input('Digite um número: '))
+soma = numero1 + numero2
+print(f'A soma entre {numero1} e {numero2} é igual a {soma}.\033[m')
