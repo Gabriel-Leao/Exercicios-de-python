5eef4fd5f2565a22bd89e36f77827b2501ec1902
Gabriel Leão da Silva <gabriellsilva1200@outlook.com.br>
2021-04-21 19:01:27 -0300

    Update ex010.py

--- Mundo-1/ex010.py
+++ Mundo-1/ex010.py
@@ -1,3 +1,5 @@
 real = float(input('\033[1;97mQuanto reais você tem? R$'))
-dolar = real / 5.57 # Essa é a cotaçaõ do dolar em 21/04/2021
-print(f'Com R${real:.2f} você pode comprar US${dolar:.2f}\033[m')
+dolar = real / 5.57 # Essa é a cotação do dolar em 21/04/2021
+euro = real / 6.70 # Essa é a cotação do euro em 21/04/2021
+libra = real / 7.76 # Essa é a cotação da libra em 21/04/2021
+print(f'Com R${real:.2f} você pode comprar US${dolar:.2f}, EUR${euro:.2f} e GBP${libra:.2f}.\033[m')
