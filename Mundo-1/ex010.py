613d75569be79335198ab61699600f59d7355d43
Gabriel Leão da Silva <gabriellsilva1200@outlook.com.br>
2021-04-21 18:53:03 -0300

    Update ex010.py

--- Mundo-1/ex010.py
+++ Mundo-1/ex010.py
@@ -1,3 +1,3 @@
 real = float(input('\033[1;97mQuanto reais você tem? R$'))
-dolar = real / 5.57
+dolar = real / 5.57 # Essa é a cotaçaõ do dolar em 21/04/2021
 print(f'Com R${real:.2f} você pode comprar US${dolar:.2f}\033[m')
