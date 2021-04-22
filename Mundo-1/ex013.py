33f3c354b86341cc7cdc2b5938db792b1c3bd637
Gabriel Leão da Silva <gabriellsilva1200@outlook.com.br>
2021-04-22 18:49:03 -0300

    Create ex013.py

---
+++ Mundo-1/ex013.py
@@ -0,0 +1,3 @@
+salario = float(input('Salário do funcionário: R$'))
+aumento = salario + (salario * (15 / 100))
+print(f'O funcionário que recebia R${salario:.2f}, com 15% de aumento vai receber R${aumento:.2f}.')
