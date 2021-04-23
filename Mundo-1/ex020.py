bdd39f9c3d4f75ada36f41740378fd5c17f72d16
Gabriel Leão da Silva <gabriellsilva1200@outlook.com.br>
2021-04-22 22:36:59 -0300

    Create ex020.py

---
+++ Mundo-1/ex020.py
@@ -0,0 +1,12 @@
+from random import shuffle
+a1 = str(input('Primeiro aluno: ')).title()
+a2 = str(input('Segundo aluno: ')).title()
+a3 = str(input('Terceiro aluno: ')).title()
+a4 = str(input('Quarto aluno: ')).title()
+lista_alunos = [a1, a2, a3, a4]
+shuffle(lista_alunos)
+print('A ordem de apresentação será:')
+print('-'*20)
+for c, v in enumerate(lista_alunos):
+    print(f'{c + 1}- {v}')
+print('-'*20)
