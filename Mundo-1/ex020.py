dd7637dcb2b60febdc8430849b6f0cccc6506c12
Gabriel Leão da Silva <gabriellsilva1200@outlook.com.br>
2021-04-23 22:28:38 -0300

    Update ex020.py

--- Mundo-1/ex020.py
+++ Mundo-1/ex020.py
@@ -5,8 +5,9 @@ a3 = str(input('Terceiro aluno: ')).title()
 a4 = str(input('Quarto aluno: ')).title()
 lista_alunos = [a1, a2, a3, a4]
 shuffle(lista_alunos)
+print('-'*30)
 print('A ordem de apresentação será:')
-print('-'*20)
+print('-'*30)
 for c, v in enumerate(lista_alunos):
     print(f'{c + 1}- {v}')
-print('-'*20)
+print('-'*30)
