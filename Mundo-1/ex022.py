ae7b8504f2928d45f4a160c14d2d81cac668b623
Gabriel Leão da Silva <gabriellsilva1200@outlook.com.br>
2021-05-14 21:24:57 -0300

    Create ex022.py

---
+++ Mundo-1/ex022.py
@@ -0,0 +1,8 @@
+nome = str(input('Digite o seu nome completo: ')).strip()
+nome_separado = nome.split()
+letras_nome = len(nome) - nome.count(' ')
+print('Analisando o seu nome...')
+print(f'Seu nome em maiúsculas é {nome.upper()}')
+print(f'Seu nome em minúsculas é {nome.lower()}')
+print(f'Seu nome tem ao todo {letras_nome} letras')
+print(f'Seu primeiro nome é {nome_separado[0]}, e ele tem um total de {len(nome_separado[0])} letras.')
