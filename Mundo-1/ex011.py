8d2dde50ffebb5da3580b0f5820cc088fe364942
Gabriel Leão da Silva <gabriellsilva1200@outlook.com.br>
2021-12-26 12:04:23 -0300

    Update ex011.py

--- Mundo-1/ex011.py
+++ Mundo-1/ex011.py
@@ -1,6 +1,6 @@
-base = float(input('\033[1;97mLargura da parede: '))
+largura = float(input('\033[1;97mLargura da parede: '))
 altura = float(input('Altura parede: '))
-area = base * altura
+area = largura * altura
 tinta = area / 2
-print(f'A sua parede tem a dimensão de {base}x{altura} e sua área é de {area}².')
+print(f'A sua parede tem a dimensão de {largura}x{altura} e sua área é de {area}².')
 print(f'Para pintar essa parede, será necessario {tinta}L de tinta.\033[m')
