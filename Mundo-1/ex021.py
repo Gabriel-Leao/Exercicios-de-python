71933d24ab938d9cd2f4d64dc784b79cb8e756d2
Gabriel Leão da Silva <gabriellsilva1200@outlook.com.br>
2021-12-26 13:01:46 -0300

    update
    update ex021

--- Mundo-1/ex021.py
+++ Mundo-1/ex021.py
@@ -1,6 +1,11 @@
 import pygame
+
+# Inicializando o mixer PyGame
 pygame.mixer.init()
+
+# Iniciando o Pygame
 pygame.init()
-pygame.mixer.music.load('ex021.mp3')
-pygame.mixer.music.play()
+
+pygame.mixer.music.load("C:/Users/gabri/Documents/GitHub/Exercicios-de-python/Mundo-1/ex021.mp3")
+pygame.mixer.music.play(loops=0, start=0.0)
 pygame.event.wait()
